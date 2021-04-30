2b358bb6e247e08b0e0c7568fd2c9163a53037f4
Rodrigo Alvarez <papipo@gmail.com>
2021-04-30 11:30:58 +0200

    Update spec to match MIT version of maglev

--- spec/lib/generators/maglev/theme_generator_spec.rb
+++ spec/lib/generators/maglev/theme_generator_spec.rb
@@ -7,15 +7,13 @@ require 'generators/maglev/theme_generator'
 describe Maglev::ThemeGenerator, type: :generator do
   destination File.expand_path('../tmp', __dir__)
 
-  arguments %w[simple]
-
   before(:all) do
     prepare_destination
     run_generator
   end
 
   it 'creates the YAML file to describe the theme and its default HTML/ERB layout' do
-    assert_file 'app/theme/theme.yml', /id: "simple"/
+    assert_file 'app/theme/theme.yml', /id: "theme"/
     assert_file 'app/views/theme/layout.html.erb'
   end
 end
