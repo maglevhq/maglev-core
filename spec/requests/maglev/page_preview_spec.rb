9336e239b4789e36d7b12af40e27147ab8dd40bd
Rodrigo Alvarez <papipo@gmail.com>
2021-05-04 12:45:31 +0200

    Fix specs

--- spec/requests/maglev/page_preview_spec.rb
+++ spec/requests/maglev/page_preview_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Maglev::PagePreview', type: :request do
-  let!(:site) { Maglev::Site.generate!(name: 'My first site') }
+  let!(:site) { Maglev::Site.generate! }
 
   context 'normal rendering' do
     it 'renders the index page' do
