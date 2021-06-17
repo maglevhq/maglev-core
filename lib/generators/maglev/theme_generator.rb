77ff0fcfea4d4bd900d0074c6f15923133e040f2
Rodrigo Alvarez <papipo@gmail.com>
2021-06-17 11:22:14 +0200

    Add hooks for generators.

--- lib/generators/maglev/theme_generator.rb
+++ lib/generators/maglev/theme_generator.rb
@@ -3,6 +3,7 @@
 module Maglev
   class ThemeGenerator < Rails::Generators::Base
     source_root File.expand_path('templates/theme', __dir__)
+    hook_for :maglev_theme
 
     def create_theme_files
       directory 'app'
