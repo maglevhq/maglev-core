12748c531cd3cead7dfac75d2e62f3188d787e16
Didier Lafforgue <didier.lafforgue@gmail.com>
2021-06-21 00:36:17 +0200

    add a test to make sure we don't break the main editor page

--- lib/tasks/maglev_tasks.rake
+++ lib/tasks/maglev_tasks.rake
@@ -3,6 +3,8 @@
 namespace :maglev do
   desc 'Create site'
   task create_site: :environment do
-    Maglev::GenerateSite.call
+    Maglev::GenerateSite.call(
+      theme: Maglev.local_themes.first
+    )
   end
 end
---
+++ spec/requests/maglev/editor_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Maglev::EditorController', type: :request do
+  let(:theme) { build(:theme, :predefined_pages) }
+  let!(:site) do
+    Maglev::GenerateSite.call(theme: theme)
+  end
+
+  it 'renders the editor page' do
+    get '/maglev/editor'
+    expect(response.body).to include('My simple theme')
+    expect(response.body).to include('window.baseUrl = "/maglev/editor"')
+    expect(response.body).to include('window.apiBaseUrl = \'/maglev/api\'')
+    expect(response.body).to include('window.site = {')
+    expect(response.body).to include('window.theme = {')
+  end
+end
