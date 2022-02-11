c88bf72792488f360cfcb17ad97d1743eff14a53
Didier Lafforgue <didier.lafforgue@gmail.com>
2022-02-11 09:55:32 +0100

    lint code

--- lib/maglev/config.rb
+++ lib/maglev/config.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Maglev
-  Config = Struct.new(:primary_color, :title, :favicon, :logo, :back_action, 
+  Config = Struct.new(:primary_color, :title, :favicon, :logo, :back_action,
                       :uploader, :asset_host, :services,
                       :collections, :is_authenticated, :ui_locale, :default_site_locales,
                       :static_pages, :reserved_paths,
