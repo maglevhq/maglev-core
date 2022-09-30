4dbe6f34d16c21174831e8434a906a5a62ccdccb
Didier Lafforgue <didier.lafforgue@gmail.com>
2022-09-30 23:33:03 +0200

    write a rails application template for quick installation

---
+++ template.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+rails_command 'db:create'
+rails_command 'active_storage:install'
+
+gem 'maglevcms', '~> 1.0.0.rc2'
+gem 'maglevcms-hyperui-kit', '~> 1.0.1'
+gem 'image_processing', '~> 1.2'
+
+after_bundle do
+  generate 'maglev:install'
+  generate 'maglev:hyperui:install', '--force'
+  
+  rails_command 'maglev:create_site'
+end
