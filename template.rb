a091f88d69ab903710a6f36702ee8f7191f42ef5
Didier Lafforgue <didier.lafforgue@gmail.com>
2022-10-06 22:16:41 +0200

    be less strict about the Rails 6 / Ruby 2.7 combo

--- template.rb
+++ template.rb
@@ -1,15 +1,5 @@
 # frozen_string_literal: true
 
-# Maglev works only with the following combinations:
-# - Rails 6 and Ruby 2.7
-# - Rails 7 and Ruby 3+
-
-if Rails.version.start_with?('6.') && RUBY_VERSION.start_with?('3.')
-  puts "🚨 We're sorry but Maglev works with Rails 6 and Ruby 2.7.x.\n
-    Please either upgrade Rails to the version 7 or use Ruby 2.7."
-  exit
-end
-
 if Rails.version.start_with?('7.') && RUBY_VERSION.start_with?('2.')
   puts "🚨 We're sorry but Maglev works with Rails 7 and Ruby 3.x.\nPlease install Ruby 3."
   exit
