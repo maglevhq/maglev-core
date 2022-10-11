964a9372cbb3287dfdc8de2737b529b3f5427dc2
Didier Lafforgue <didier.lafforgue@gmail.com>
2022-10-11 16:05:42 +0200

    bump version to 1.0.0 🚀🎉😎

--- lib/maglev/version.rb
+++ lib/maglev/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Maglev
-  VERSION = '1.0.0.rc3'
+  VERSION = '1.0.0'
 end
--- template.rb
+++ template.rb
@@ -5,7 +5,7 @@ if Rails.version.start_with?('7.') && RUBY_VERSION.start_with?('2.')
   exit
 end
 
-gem 'maglevcms', '~> 1.0.0.rc3'
+gem 'maglevcms', '~> 1.0.0'
 gem 'maglevcms-hyperui-kit', '~> 1.0.1'
 gem 'image_processing', '~> 1.2'
 
