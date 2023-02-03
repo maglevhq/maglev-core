f4a0f794f58bc2b8483cf3551a420845839c859b
Hasan Iskandar <hasan.iskandar@gmail.com>
2023-02-04 05:00:26 +0700

    fix link value

--- spec/lib/generators/maglev/section_generator_spec.rb
+++ spec/lib/generators/maglev/section_generator_spec.rb
@@ -7,6 +7,7 @@ require 'generators/maglev/section_generator'
 BLOCK_SETTINGS = %w[
   title:text
   pre_title:text
+  call_to_action:link
   block:item:title
   block:item:image:image
   block:item:description
@@ -25,5 +26,13 @@ describe Maglev::SectionGenerator, type: :generator do
   it 'creates the YAML file to describe the section and its HTML/ERB template' do
     assert_file 'app/theme/sections/features/showcase.yml', /name: "Showcase"/
     assert_file 'app/views/theme/sections/features/showcase.html.erb'
+  end
+
+  it 'provides default link' do
+    assert_file 'app/theme/sections/features/showcase.yml', /default: { text: "Link", href: "#" }/
+  end
+
+  it 'provides sample link' do
+    assert_file 'app/theme/sections/features/showcase.yml', /call_to_action: { text: "Link", href: "#" }/
   end
 end
