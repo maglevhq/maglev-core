921ab6695e0362cfe7f347657146175c3cdb6707
Didier Lafforgue <didier.lafforgue@gmail.com>
2023-06-20 09:39:23 +0200

    lint code

--- app/controllers/maglev/sitemap_controller.rb
+++ app/controllers/maglev/sitemap_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Maglev
   class SitemapController < ApplicationController
     include Maglev::FetchersConcern
--- spec/requests/maglev/sitemap_spec.rb
+++ spec/requests/maglev/sitemap_spec.rb
@@ -16,28 +16,28 @@ RSpec.describe 'Maglev::SitemapController', type: :request do
     it 'renders a XML output' do
       get '/sitemap.xml', as: :xml
       expect(response.body.strip).to eq(<<~XML
-      <?xml version="1.0" encoding="UTF-8"?>
-      <urlset xmlns="http://www.google.com/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
-        <url>
-          <loc>http://www.example.com/</loc>
-          <lastmod>#{Time.zone.now.strftime('%Y-%m-%d')}</lastmod>
-          <xhtml:link rel="alternate" hreflang="en" href="http://www.example.com/"/>
-          <xhtml:link rel="alternate" hreflang="fr" href="http://www.example.com/fr"/>
-        </url>
-        <url>
-          <loc>http://www.example.com/about-us</loc>
-          <lastmod>#{Time.zone.now.strftime('%Y-%m-%d')}</lastmod>
-          <xhtml:link rel="alternate" hreflang="en" href="http://www.example.com/about-us"/>
-          <xhtml:link rel="alternate" hreflang="fr" href="http://www.example.com/fr/about-us"/>
-        </url>
-        <url>
-          <loc>http://www.example.com/empty</loc>
-          <lastmod>#{Time.zone.now.strftime('%Y-%m-%d')}</lastmod>
-          <xhtml:link rel="alternate" hreflang="en" href="http://www.example.com/empty"/>
-          <xhtml:link rel="alternate" hreflang="fr" href="http://www.example.com/fr/empty"/>
-        </url>
-      </urlset>
-    XML
+        <?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.google.com/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
+          <url>
+            <loc>http://www.example.com/</loc>
+            <lastmod>#{Time.zone.now.strftime('%Y-%m-%d')}</lastmod>
+            <xhtml:link rel="alternate" hreflang="en" href="http://www.example.com/"/>
+            <xhtml:link rel="alternate" hreflang="fr" href="http://www.example.com/fr"/>
+          </url>
+          <url>
+            <loc>http://www.example.com/about-us</loc>
+            <lastmod>#{Time.zone.now.strftime('%Y-%m-%d')}</lastmod>
+            <xhtml:link rel="alternate" hreflang="en" href="http://www.example.com/about-us"/>
+            <xhtml:link rel="alternate" hreflang="fr" href="http://www.example.com/fr/about-us"/>
+          </url>
+          <url>
+            <loc>http://www.example.com/empty</loc>
+            <lastmod>#{Time.zone.now.strftime('%Y-%m-%d')}</lastmod>
+            <xhtml:link rel="alternate" hreflang="en" href="http://www.example.com/empty"/>
+            <xhtml:link rel="alternate" hreflang="fr" href="http://www.example.com/fr/empty"/>
+          </url>
+        </urlset>
+      XML
 .strip)
     end
   end
