6b841e022e44d8e2a3fb2316bd7c667b93eda86b
Rafael Iga <rafael.f.iga@gmail.com>
2023-10-04 20:15:06 -0300

    feat: adds pt-BR translations

--- app/frontend/editor/locales/index.js
+++ app/frontend/editor/locales/index.js
@@ -1,14 +1,17 @@
 import EditorEN from './editor.en.json'
 import EditorES from './editor.es.json'
 import EditorFR from './editor.fr.json'
+import EditorPTBR from './editor.pt-BR.json'
 import { deepMerge } from '@/misc/utils'
 
 const overriddenEN = window.customTranslations?.en ?? {}
 const overriddenES = window.customTranslations?.es ?? {}
 const overriddenFR = window.customTranslations?.fr ?? {}
+const overriddenPTBR = (window.customTranslations && window.customTranslations['pt-BR']) ? window.customTranslations['pt-BR'] : {}
 
 export default {
   en: deepMerge(EditorEN, overriddenEN),
   es: deepMerge(EditorES, overriddenES),
   fr: deepMerge(EditorFR, overriddenFR),
+  "pt-BR": deepMerge(EditorPTBR, overriddenPTBR),
 }
