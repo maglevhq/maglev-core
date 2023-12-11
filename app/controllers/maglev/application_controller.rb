3fe58370b755d34b954e8efbc5633889c23358f8
Didier Lafforgue <didier.lafforgue@gmail.com>
2023-12-11 08:49:57 +0100

    docs: explain why we picked the first 35 characters of the id param (when PG is setup with uuids as primary keys)

--- app/controllers/maglev/application_controller.rb
+++ app/controllers/maglev/application_controller.rb
@@ -15,6 +15,7 @@ module Maglev
     end
 
     def resource_id
+      # A standard UUID code contains 32 hex digits along with 4 "-"" symbols
       Maglev.uuid_as_primary_key? && params[:id] ? params[:id][0..35] : params[:id]
     end
   end
