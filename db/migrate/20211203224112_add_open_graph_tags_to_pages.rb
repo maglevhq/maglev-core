55b1efb096ee2d6b03a968e7534e5171693a4da1
Henrik Hauge Bjørnskov <19725+henrikbjorn@users.noreply.github.com>
2025-03-06 12:03:52 +0100

    Check if jsonb type is available in migrations. Otherwise fallback to json. This make SQLite supported

--- db/migrate/20211203224112_add_open_graph_tags_to_pages.rb
+++ db/migrate/20211203224112_add_open_graph_tags_to_pages.rb
@@ -1,9 +1,15 @@
 class AddOpenGraphTagsToPages < ActiveRecord::Migration[6.0]
   def change
     change_table :maglev_pages do |t|
-      t.jsonb :og_title_translations, default: {}
-      t.jsonb :og_description_translations, default: {}
-      t.jsonb :og_image_url_translations, default: {}
+      if t.respond_to? :jsonb
+        t.jsonb :og_title_translations, default: {}
+        t.jsonb :og_description_translations, default: {}
+        t.jsonb :og_image_url_translations, default: {}
+      else
+        t.json :og_title_translations, default: {}
+        t.json :og_description_translations, default: {}
+        t.json :og_image_url_translations, default: {}
+      end
     end
   end
 end
