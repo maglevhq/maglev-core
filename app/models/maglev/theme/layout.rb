2f680b614a08c5cb3f0f9eafecd873107a2f1dad
Didier Lafforgue <ext-didier.lafforgue@swile.co>
2025-04-13 12:40:11 +0200

    chore: prepare the upgrade to v2 task (WIP)

--- app/models/maglev/theme/layout.rb
+++ app/models/maglev/theme/layout.rb
@@ -16,5 +16,13 @@ class Maglev::Theme::Layout < Maglev::Theme::BaseProperty
 
     new(attributes)
   end
+
+  def self.build_many(list)
+    super(list.presence || [default_layout])
+  end
+
+  def self.default_layout
+    { id: 'default', label: 'Default', groups: [Maglev::Theme::LayoutGroup.default_group] }.with_indifferent_access
+  end
 end
 # rubocop:enable Style/ClassAndModuleChildren
