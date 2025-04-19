29b00490a21b8cabe2be3c0dd682b3bf422bb1c7
Didier Lafforgue <didier.lafforgue@gmail.com>
2025-04-19 17:16:49 +0200

    Support SQLite3 (#129)
    * Check if jsonb type is available in migrations. Otherwise fallback to json. This make SQLite supported
    * Run against sqlite also in the CI

--- db/migrate/20200831101942_create_maglev_section_content.rb
+++ db/migrate/20200831101942_create_maglev_section_content.rb
@@ -1,11 +1,19 @@
 class CreateMaglevSectionContent < ActiveRecord::Migration[6.0]
   def change
     change_table :maglev_sites do |t|
-      t.jsonb :sections, default: []
+      if t.respond_to? :jsonb
+        t.jsonb :sections, default: []
+      else
+        t.json :sections, default: []
+      end
     end
 
     change_table :maglev_pages do |t|
-      t.jsonb :sections, default: []
+      if t.respond_to? :jsonb
+        t.jsonb :sections, default: []
+      else
+        t.json :sections, default: []
+      end
     end
   end
 end
--- db/migrate/20210819092740_switch_to_localized_page_fields.rb
+++ db/migrate/20210819092740_switch_to_localized_page_fields.rb
@@ -1,15 +1,25 @@
 class SwitchToLocalizedPageFields < ActiveRecord::Migration[6.1]
   def up
     remove_columns :maglev_pages, :title, :seo_title, :meta_description
-    add_column :maglev_pages, :title_translations, :jsonb, default: {}
-    add_column :maglev_pages, :seo_title_translations, :jsonb, default: {}
-    add_column :maglev_pages, :meta_description_translations, :jsonb, default: {}
+
+    change_table :maglev_pages do |t|
+      if t.respond_to? :jsonb
+        t.jsonb :title_translations, default: {}
+        t.jsonb :seo_title_translations, default: {}
+        t.jsonb :meta_description_translations, default: {}
+      else
+        t.json :title_translations, default: {}
+        t.json :seo_title_translations, default: {}
+        t.json :meta_description_translations, default: {}
+      end
+    end
   end
 
   def down
     add_column :maglev_pages, :title, :string
     add_column :maglev_pages, :seo_title, :string
     add_column :maglev_pages, :meta_description, :string
+
     remove_column :maglev_pages, :title_translations
     remove_column :maglev_pages, :seo_title_translations
     remove_column :maglev_pages, :meta_description_translations
