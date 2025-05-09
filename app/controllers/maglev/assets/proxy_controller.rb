1ba6d061d3d036ad2396ac0d0ad106976dc32931
Didier Lafforgue <ext-didier.lafforgue@swile.co>
2025-05-09 18:30:34 +0200

    fix: deal with the different behaviours regarding public_file_server headers (Rails 7.x & Rails 8+)

--- app/controllers/maglev/assets/proxy_controller.rb
+++ app/controllers/maglev/assets/proxy_controller.rb
@@ -6,7 +6,15 @@ module Maglev
       def show
         @asset = Maglev::Asset.find(resource_id)
         send_data @asset.download, filename: @asset.filename, type: @asset.content_type
-        response.headers['Cache-Control'] = Rails.configuration.public_file_server.headers[:'cache-control']
+        response.headers['Cache-Control'] = cache_control_header
+      end
+
+      private
+
+      def cache_control_header
+        # Rails 8+ uses :'cache-control' and Rails 7.x uses :'Cache-Control'
+        Rails.configuration.public_file_server.headers[:'cache-control'] ||
+          Rails.configuration.public_file_server.headers[:'Cache-Control']
       end
     end
   end
--- spec/requests/maglev/assets/active_storage_proxy_spec.rb
+++ spec/requests/maglev/assets/active_storage_proxy_spec.rb
@@ -8,6 +8,6 @@ RSpec.describe 'Maglev::Assets::ActiveStorageProxyController' do
   it 'allows retrieval by id' do
     get "/maglev/assets/#{asset.id}/gibberish"
     expect(response.body).to eq(IO.binread(file_fixture('asset.jpg')))
-    expect(response.headers['cache-control']).to eq('max-age=3155695200, public, immutable')
+    expect(response.headers['cache-control']).to match('max-age=3155695200, public')
   end
 end
