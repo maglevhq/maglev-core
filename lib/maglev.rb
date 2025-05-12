fdd9e6fd60ffc3face7968778888d4d0a97ebb81
Mario Young <maye.co@gmail.com>
2025-05-11 23:03:05 -0400

    Change to symbol for default settings

--- lib/maglev.rb
+++ lib/maglev.rb
@@ -26,7 +26,7 @@ module Maglev
         c.favicon = nil
         c.logo = nil
         c.primary_color = '#040712'
-        c.uploader = 'active_storage'
+        c.uploader = :active_storage
         c.site_publishable = false
         c.preview_host = nil
         c.asset_host = Rails.application.config.action_controller.asset_host
