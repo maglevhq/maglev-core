7bb470f2f5917bfa36bdefd2c314d16856eabd31
Didier Lafforgue <ext-didier.lafforgue@swile.co>
2025-05-17 00:25:43 +0200

    feat: use a trash bin icon as the delete action icon

--- app/frontend/editor/components/kit/list-item-button.vue
+++ app/frontend/editor/components/kit/list-item-button.vue
@@ -1,8 +1,8 @@
 <template>
   <button
-    class="px-1 py-1 rounded-full bg-gray-600 bg-opacity-0 hover:text-gray-900 text-gray-600 focus:outline-none hover:bg-opacity-10 transition-colors duration-200"
+    class="px-1.5 py-1.5 rounded-full bg-gray-600 bg-opacity-0 hover:text-gray-900 text-gray-700 focus:outline-none hover:bg-opacity-10 transition-colors duration-200"
   >
-    <uikit-icon :name="iconName" size="1.25rem" />
+    <uikit-icon :name="iconName" size="1.15rem" />
   </button>
 </template>
 
--- app/frontend/editor/components/section-list/list-item.vue
+++ app/frontend/editor/components/section-list/list-item.vue
@@ -12,11 +12,7 @@
       @confirm="removeSection(section.id)"
       v-on="$listeners"
     >
-      <button
-        class="px-1 py-1 rounded-full bg-gray-600 bg-opacity-0 hover:text-gray-900 text-gray-600 focus:outline-none hover:bg-opacity-10 transition-colors duration-200"
-      >
-        <uikit-icon name="ri-close-line" size="1.25rem" />
-      </button>
+      <uikit-list-item-button iconName="delete-bin-line" />      
     </uikit-confirmation-button>
   </div>
 </template>
