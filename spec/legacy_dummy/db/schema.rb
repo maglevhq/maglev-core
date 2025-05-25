a51a432a1e4c5c2e492d8e5bc49ca2da61692855
Didier Lafforgue <ext-didier.lafforgue@swile.co>
2025-05-25 13:37:36 +0200

    fix(ci): the legacy dummy app DB schema must be Rails 7 compliant

--- spec/legacy_dummy/db/schema.rb
+++ spec/legacy_dummy/db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 20_220_612_092_235) do
+ActiveRecord::Schema.define(version: 20_220_612_092_235) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
