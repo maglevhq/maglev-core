fff70529ecf5889e16b1172fcb1eb98927f6c174
Didier Lafforgue <ext-didier.lafforgue@swile.co>
2025-05-29 21:10:11 +0200

    feat(ui): less prominent page icon

--- app/frontend/editor/components/header-nav/page-info.vue
+++ app/frontend/editor/components/header-nav/page-info.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="flex items-center h-full px-4 flex-row w-full overflow-hidden">
-    <uikit-page-icon :page="currentPage" size="1.75rem" class="shrink-0" />
-    <div class="ml-4 flex flex-col leading-none overflow-hidden">
-      <div class="text-base font-semibold truncate">{{ currentPage.title }}</div>
-      <div class="text-xs text-gray-500 -mt-0.5 flex items-center space-x-1">
-        <span class="truncate">{{ currentPagePath }}</span>
+  <div class="flex flex-row items-center h-full w-full px-4 overflow-hidden">
+    <div class="flex flex-col leading-none overflow-hidden">
+      <div class="flex items-center space-x-1">
+        <uikit-page-icon :page="currentPage" size="1.1rem" class="shrink-0 text-gray-900" />
+        <div class="text-base font-semibold truncate">{{ currentPage.title }}</div>
+      </div>
+      <div class="text-xs text-gray-500 flex items-center space-x-1">
+        <p class="truncate text-gray-700"><span class="text-gray-900 font-semibold">{{ currentPagePath[0] }}</span>{{ currentPagePath.slice(1) }}</p>
         <uikit-copy-paste-button :text-to-copy="currentPageUrl" />
       </div>
     </div>
