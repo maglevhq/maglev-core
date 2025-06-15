2284bfad818bd4a99337fe172ab0b1dae1c84f11
Didier Lafforgue <ext-didier.lafforgue@swile.co>
2025-06-15 23:14:56 +0200

    feat(ui): revamp the list item components (icon + label)

--- app/frontend/editor/components/section-list/list-item.vue
+++ app/frontend/editor/components/section-list/list-item.vue
@@ -1,16 +1,16 @@
 <template>
   <div
-    class="bg-gray-100 rounded-md pr-4 flex items-center text-gray-800"
+    class="bg-gray-100 rounded-md pr-2 flex items-center text-gray-800"
   >
-    <div class="flex flex-col cursor-move px-4 py-3">
-      <uikit-icon name="ri-expand-up-down-line" />
+    <div class="flex flex-col cursor-move px-2 py-3">
+      <uikit-icon name="ri-draggable" />
     </div>
     <router-link
       :to="{ name: 'editSection', params: { sectionId: section.id } }"
-      class="flex flex-col overflow-hidden py-3"
+      class="flex flex-col overflow-hidden py-3 pr-2 leading-none"
     >
-      <span :class="{ 'text-gray-800': label }">{{ name | truncate(40) }}</span>
-      <span class="text-gray-500 text-sm truncate" v-if="label">{{ label | truncate(100) }}</span>
+      <span :class="{ 'text-gray-500 text-sm': label }">{{ name | truncate(40) }}</span>
+      <span class="text-gray-800 truncate" v-if="label">{{ label | truncate(100) }}</span>
     </router-link>
     <uikit-confirmation-button
       @confirm="removeSection(section.id)"
