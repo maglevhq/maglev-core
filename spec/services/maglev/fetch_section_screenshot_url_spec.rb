d19dbdffda48a3b2bf57b1cc4ca6a98594d0e907
Didier Lafforgue <didier.lafforgue@gmail.com>
2025-08-22 12:14:26 +0200

    feat: use the Rails asset host to generate the URL of the static section screenshots (#187)

--- spec/services/maglev/fetch_section_screenshot_url_spec.rb
+++ spec/services/maglev/fetch_section_screenshot_url_spec.rb
@@ -13,4 +13,14 @@ describe Maglev::FetchSectionScreenshotUrl do
   it 'returns the url to the screenshot of the section' do
     expect(subject).to eq '/theme/jumbotron.png?42'
   end
+
+  context 'when there is a Rails asset host set' do
+    before do
+      allow(Rails.application.config).to receive(:asset_host).and_return('https://assets.maglev.local')
+    end
+
+    it 'uses the Rails asset host to build the url' do
+      expect(subject).to eq 'https://assets.maglev.local/theme/jumbotron.png?42'
+    end
+  end
 end
