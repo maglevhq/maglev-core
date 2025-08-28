74dca0e21597ca1d5f0c856b703fd82b1310e6c3
Did <didier.lafforgue@gmail.com>
2025-08-29 00:55:53 +0200

    chore(ux): the page preview behavior is iso with the VueJS version (scaling of  the iframe)

--- app/assets/javascripts/maglev/editor/controllers/page_preview_controller.js
+++ app/assets/javascripts/maglev/editor/controllers/page_preview_controller.js
@@ -1,26 +1,56 @@
 import { Controller } from "@hotwired/stimulus"
+import { useDebounce } from 'stimulus-use'
 
 export default class extends Controller {
   static values = { primaryColor: String }
   static targets = ["loading", "iframe"]
 
+  // static debounces = ['calculateTransformations']
+
   connect() {
-    console.log('PagePreviewController', this.urlValue)
-    this.iframeTarget.addEventListener('load', this.iframeLoaded.bind(this))
+    // useDebounce(this)
+    this.setupTransformations()
   }
 
   disconnect() {
     console.log('PagePreviewController disconnect')
+    this.teardownTransformations()
   }
 
+  // --- device management ---
   changeDevice(event) {
     this.element.dataset.device = event.detail.device
+    this.calculateTransformations()
   }
 
+  isTabletDevice() {
+    return this.element.dataset.device === 'tablet'
+  }
+
+  isMobileDevice() {
+    return this.element.dataset.device === 'mobile'
+  }
+
+  // called when the user is being redirected to a new page in the editor (another Maglev page OR in a different locale) 
+  // It displays the splash screen (the loading message). Required for a good UX.
   startLoading() {
     this.element.classList.remove('is-loaded')
   }
 
+  // called when the iframe DOM is loaded
+  iframeLoaded() {
+    this.postMessage('config', {
+      primaryColor: this.primaryColorValue,
+      stickySectionIds: [], // TODO: get the sticky section ids from the page, use a JSON value
+    })
+  }
+
+  // called when the Maglev client JS lib has been fully loaded on the iframe
+  clientReady() {
+    this.element.classList.add('is-loaded')
+  }
+
+  // called when the user navigates to a new page in the editor (another Maglev page OR in a different locale) 
   detectUrlChange() {
     const currentPath = new URL(this.iframeTarget.src).pathname
     const newPath = document.querySelector('meta[name=page-preview-url]').content
@@ -28,18 +58,95 @@ export default class extends Controller {
     if (currentPath !== newPath) this.iframeTarget.src = newPath
   }
 
-  iframeLoaded() {
-    this.postMessage('config', {
-      primaryColor: this.primaryColorValue,
-      stickySectionIds: [], // TODO: get the sticky section ids from the page
-    })
-  }
-
-  clientReady() {
-    this.element.classList.add('is-loaded')
-  }
-
+  // Used to communicate with the iframe
   postMessage(type, data) {
     this.iframeTarget.contentWindow.postMessage({ type, ...(data || {}) }, '*')
+  }
+
+  // --- Transformation utilities ---
+
+  setupTransformations() {
+    this.calculateTransformations = this.calculateTransformations.bind(this)
+    this.ro = new ResizeObserver(this.calculateTransformations)
+    
+    const sidebar = this.appLayoutSidebar
+    const pageLayout = document.querySelector('#page-layout')
+    if (sidebar) this.ro.observe(this.appLayoutSidebar())
+    if (pageLayout) this.ro.observe(pageLayout)
+    this.ro.observe(document.documentElement)
+
+    // Recompute on page layout toggles / VT renders
+    addEventListener("turbo:render", this.calculateTransformations)
+    addEventListener("turbo:before-render", this.calculateTransformations)
+
+    this.calculateTransformations()
+  }
+
+  teardownTransformations() {
+    this.ro?.disconnect()
+    removeEventListener("turbo:render", this.calculateTransformations)
+    removeEventListener("turbo:before-render", this.calculateTransformations)
+  }
+
+  expandedPageLayout() {
+    return document.querySelector('#page-layout[data-expanded]')
+  }
+
+  appLayoutSidebar() {
+    return document.querySelector('#app-layout-sidebar')
+  }
+
+  previewMaxWidth() {
+    return window.innerWidth - (this.expandedPageLayout()?.offsetWidth || 0)
+  }
+
+  appLayoutSidebarWidth() {
+    return this.appLayoutSidebar()?.offsetWidth || 0
+  }
+
+  hasExpandedPageLayout() {
+    return !!this.expandedPageLayout()
+  }
+
+  hasEnoughWidthForTablet() {
+    return this.isTabletDevice() && this.previewMaxWidth() > 1024
+  }
+  
+  hasEnoughWidthForMobile() {
+    return this.isMobileDevice() && this.previewMaxWidth() > 375
+  }
+
+  calculateTransformations() {
+    // console.log('calculateTransformations 🍔', this.calculateTransformX(), this.calculateScaleRatio())
+    this.element.style.setProperty('--page-preview-transform-x', `${this.calculateTransformX()}px`)
+    this.element.style.setProperty('--page-preview-scale-ratio', `${this.calculateScaleRatio()}`)
+
+    if (this.expandedPageLayout() && this.calculateScaleRatio() === 1) {
+      this.element.style.setProperty('--page-preview-width', `${this.previewMaxWidth()}px`)
+    } else {
+      this.element.style.setProperty('--page-preview-width', '100%')
+    }
+  }
+
+  calculateTransformX() {
+    if (!this.hasExpandedPageLayout()) return 0
+    return this.expandedPageLayout().offsetWidth - this.appLayoutSidebarWidth()
+  }
+
+  calculateScaleRatio() {
+    if (!this.mustApplyScale()) return 1
+    return this.previewMaxWidth() / (window.innerWidth - this.appLayoutSidebarWidth())
+  }
+
+  mustApplyScale() {
+    if (!this.hasExpandedPageLayout()) return false
+
+     // case: there is enough width for the tablet viewport, no need to scale down
+     if (this.hasEnoughWidthForTablet()) return false
+
+     // case: there is enough width for the mobile viewport, no need to scale down
+     if (this.hasEnoughWidthForMobile()) return false
+
+    return true
   }
 }
--- app/components/maglev/uikit/page_layout_component/page_layout_controller.js
+++ app/components/maglev/uikit/page_layout_component/page_layout_controller.js
@@ -6,13 +6,20 @@ export default class extends Controller {
     const oldDoc = document
     const newBody = event.detail.newBody
 
-    const hadAppLayout = !!oldDoc.querySelector('#drawer-component')
-    const hasAppLayout = !!newBody.querySelector('#drawer-component')
+    const hadPageLayout = !!oldDoc.querySelector('#page-layout')
+    const hasPageLayout = !!newBody.querySelector('#page-layout')
+    const hasExpandedPageLayout = !!newBody.querySelector('#page-layout[data-expanded]')
 
-    if (hadAppLayout && hasAppLayout) {
-      document.documentElement.dataset.appLayoutVtPair = "already-present"
+    if (hasExpandedPageLayout) {
+      document.documentElement.dataset.expandedPageLayout = true
     } else {
-      delete document.documentElement.dataset.appLayoutVtPair
+      delete document.documentElement.dataset.expandedPageLayout
+    }
+
+    if (hadPageLayout && hasPageLayout) {
+      document.documentElement.dataset.pageLayoutVtPair = "already-present"
+    } else {
+      delete document.documentElement.dataset.pageLayoutVtPair
     }
   }
 }
