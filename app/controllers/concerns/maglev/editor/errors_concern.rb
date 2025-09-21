d7e95a958eaf6557015ade21d7a8c55bf485b5f3
Did <didier.lafforgue@gmail.com>
2025-09-21 22:20:06 +0200

    chore: add a modal for NotAuthorized error chore: translate the error modals

--- app/controllers/concerns/maglev/editor/errors_concern.rb
+++ app/controllers/concerns/maglev/editor/errors_concern.rb
@@ -7,14 +7,22 @@ module Maglev
 
       included do
         rescue_from ActiveRecord::StaleObjectError, with: :handle_stale_object
+        rescue_from Maglev::Errors::NotAuthorized, with: :handle_not_authorized
       end
 
       private
 
       def handle_stale_object
         respond_to do |format|
-          format.turbo_stream { render 'maglev/editor/shared/stale_object_error' }
-          format.html { redirect_to request.path, alert: 'Someone else has updated this content. Please reload.' }
+          format.turbo_stream { render 'maglev/editor/shared/errors/stale_object_error' }
+          format.html { redirect_to editor_root_path, alert: 'Someone else has updated this content. Please reload.' }
+        end
+      end
+
+      def handle_not_authorized
+        respond_to do |format|
+          format.turbo_stream { render 'maglev/editor/shared/errors/not_authorized_error' }
+          format.html { redirect_to main_app.root_path, alert: 'You are not authorized to access this content.' }
         end
       end
     end
