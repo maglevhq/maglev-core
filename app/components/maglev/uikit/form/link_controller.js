cabdd635a09270771d10d80f83948c589643a234
Did <didier.lafforgue@gmail.com>
2025-10-19 23:04:08 +0200

    fix(ui): clicking on a link gives the focus on the field in the left sidebar

--- app/components/maglev/uikit/form/link_controller.js
+++ app/components/maglev/uikit/form/link_controller.js
@@ -1,7 +1,10 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = ['inputItem', 'emptyItem']
+  static targets = ['inputItem', 'emptyItem', 'textInput']
+  static values = {
+    path: String
+  }
 
   clear() {
     this.element.classList.remove('is-present')
@@ -16,5 +19,18 @@ export default class extends Controller {
   onLinkSelected(event) {
     this.element.classList.add('is-present')
     this.dispatch('change', { detail: { value: event.detail } })
+  }
+
+  openModal() {
+    const frame = document.querySelector('turbo-frame[id="modal"]')
+    frame.src = this.pathValue
+  }
+
+  focus() {
+    if (this.hasTextInputTarget) {
+      this.textInputTarget.focus()
+    } else {
+      this.openModal()
+    }
   }
 }
