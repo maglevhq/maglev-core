c15cf0a118708456e30eb003dda3c8b28597541d
Did <didier.lafforgue@gmail.com>
2026-02-11 17:04:36 +0100

    feat: different implementation for the HasUnpublishedChanges service

--- spec/services/maglev/has_unpublished_changes_spec.rb
+++ spec/services/maglev/has_unpublished_changes_spec.rb
@@ -3,34 +3,51 @@
 require 'rails_helper'
 
 describe Maglev::HasUnpublishedChanges do
+  let(:site) { build(:site) }
   let(:service) { described_class.new }
-  let(:site) { create(:site) }
-  let(:page) { create(:page) }
   let(:theme) { Maglev.local_themes.first }
+  let(:page) { create(:page) }
 
   subject { service.call(site: site, page: page, theme: theme) }
 
-  context 'neither page nor site have been published' do
+  context 'page has never been published' do
     it { is_expected.to be true }
   end
 
-  context 'both page and site are published and up to date' do
-    let(:site) { create(:site, published_at: 1.minute.from_now) }
+  context 'page is published and all stores are up to date' do
     let(:page) { create(:page, :published, published_at: 1.minute.from_now) }
+
+    before do
+      # create published counterparts for global stores
+      create(:sections_content_store, :header, :published, updated_at: 1.minute.from_now)
+      create(:sections_content_store, :site_scoped, :empty, :published, updated_at: 1.minute.from_now)
+    end
 
     it { is_expected.to be false }
   end
 
-  context 'site has unpublished changes' do
-    let(:site) { create(:site, published_at: 1.day.ago) }
-    let(:page) { create(:page, :published, published_at: 1.minute.from_now) }
+  context 'page metadata has unpublished changes' do
+    let(:page) { create(:page, :published, published_at: 1.day.ago) }
 
     it { is_expected.to be true }
   end
 
-  context 'page has unpublished changes' do
-    let(:site) { create(:site, published_at: 1.minute.from_now) }
-    let(:page) { create(:page, :published, published_at: 1.day.ago) }
+  context 'a global store has unpublished changes' do
+    let(:page) { create(:page, :published, published_at: 1.minute.from_now) }
+
+    before do
+      create(:sections_content_store, :header, published: false)
+    end
+
+    it { is_expected.to be true }
+  end
+
+  context 'the site-scoped store has unpublished changes' do
+    let(:page) { create(:page, :published, published_at: 1.minute.from_now) }
+
+    before do
+      create(:sections_content_store, :site_scoped, :empty, published: false)
+    end
 
     it { is_expected.to be true }
   end
