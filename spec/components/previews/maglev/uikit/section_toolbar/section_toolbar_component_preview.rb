e9f0fd3684cbbaf93180972fc28f1caf3ba53ad5
Did <didier.lafforgue@gmail.com>
2026-04-12 12:39:19 +0200

    fix: solve "No route matches"\ error in production

--- spec/components/previews/maglev/uikit/section_toolbar/section_toolbar_component_preview.rb
+++ spec/components/previews/maglev/uikit/section_toolbar/section_toolbar_component_preview.rb
@@ -4,12 +4,21 @@ module Maglev
   module Uikit
     module SectionToolbar
       class SectionToolbarComponentPreview < ViewComponent::Preview
+        # All keys the toolbar uses must be set; nil URLs make link_to call url_for({}) and break under Lookbook at "/".
+        TOOLBAR_PREVIEW_PATHS = {
+          show: "#",
+          edit: "#",
+          delete: "#",
+          add: "#",
+          blocks: "#"
+        }.freeze
+
         def default
-          render_with_template
+          render_with_template(locals: { preview_paths: TOOLBAR_PREVIEW_PATHS })
         end
 
         def with_preview_ratio_scale
-          render_with_template
+          render_with_template(locals: { preview_paths: TOOLBAR_PREVIEW_PATHS })
         end
       end
     end
