6d0f9a1f474b79707c10c05b07220aa4b3cb50c2
Did <didier.lafforgue@gmail.com>
2026-04-12 12:47:33 +0200

    fix: solve "No route matches"\ error in production

--- spec/components/previews/maglev/uikit/page_layout_component_preview.rb
+++ spec/components/previews/maglev/uikit/page_layout_component_preview.rb
@@ -3,24 +3,27 @@
 module Maglev
   module Uikit
     class PageLayoutComponentPreview < ViewComponent::Preview
+      # Top-right close uses link_to(back_path); nil becomes url_for({}) and breaks under Lookbook at "/".
+      PAGE_LAYOUT_PREVIEW_LOCALS = { page_layout_back_path: "#" }.freeze
+
       def default
-        render_with_template
+        render_with_template(locals: PAGE_LAYOUT_PREVIEW_LOCALS)
       end
 
       def with_footer
-        render_with_template
+        render_with_template(locals: PAGE_LAYOUT_PREVIEW_LOCALS)
       end
 
       def with_breadcrumbs
-        render_with_template
+        render_with_template(locals: PAGE_LAYOUT_PREVIEW_LOCALS)
       end
 
       def with_notification
-        render_with_template
+        render_with_template(locals: PAGE_LAYOUT_PREVIEW_LOCALS)
       end
 
       def with_back_link
-        render_with_template
+        render_with_template(locals: PAGE_LAYOUT_PREVIEW_LOCALS)
       end
     end
   end
