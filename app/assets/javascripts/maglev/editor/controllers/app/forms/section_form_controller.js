aeae2995b2748f364e15cec45d40af196254b6a0
Did <didier.lafforgue@gmail.com>
2026-07-21 23:33:40 +0200

    fix: prevent overlapping autosaves from sending a stale lock_version

--- app/assets/javascripts/maglev/editor/controllers/app/forms/section_form_controller.js
+++ app/assets/javascripts/maglev/editor/controllers/app/forms/section_form_controller.js
@@ -9,14 +9,23 @@ export default class extends Controller {
   static debounces = ['afterSettingUpdate']
 
   connect() {
-    useDebounce(this)    
+    useDebounce(this)
+    
     this.sourceId = this.sectionBlockIdValue !== '' ?  this.sectionBlockIdValue : this.sectionIdValue
+    
     requestAnimationFrame(() => {
       this.dispatch('connected', { bubbles: true, detail: { 
         sectionId: this.sectionIdValue, 
           sectionBlockId: this.sectionBlockIdValue, 
           lockVersion: this.sectionLockVersionValue } 
         })
+    })
+
+    // we need to keep track of the submitting state to avoid multiple requests being sent with a wrong lock version
+    this.submitting = false
+    this.element.addEventListener("turbo:submit-start", () => this.submitting = true)    
+    this.element.addEventListener('turbo:submit-end', (event) => {
+      if (!event.detail.success) this.submitting = false
     })
   }
 
@@ -33,15 +42,29 @@ export default class extends Controller {
   onPersist(event) {
     log('[SectionForm::onPersist]', event.detail, typeof event.detail)
     this.lockVersionTarget.value = event.detail.lockVersion
+
+    // we are done submitting, so we can reset the submitting flag
+    this.submitting = false
   }
 
-  async afterSettingUpdate(event) {
-    log('[SectionForm::afterSettingUpdate] 🙌🙌', event.detail)
-    await this.element.requestSubmit()
-    
-    if (!event.detail.updated) {
-      // we couldn't update the content just by modifying the DOM, so we have to refresh the whole section
-      this.dispatch('updateSection', { detail: { sectionId: this.sectionIdValue } })
+  afterSettingUpdate(event) {    
+    // if the request is still running (flight), we need to debounce the call
+    // to avoid multiple requests being sent with a wrong lock version
+    if (this.submitting) {
+      this.afterSettingUpdate(event)
+      return
     }
+
+    log('[SectionForm::afterSettingUpdate] 🙌🙌', event.detail)
+
+    // we couldn't update the content just by modifying the DOM, so we have to refresh the whole section.
+    // The right time to do this is when the request is finished, so we listen for the turbo:submit-end event.
+    if (!event.detail.updated) {
+      this.element.addEventListener('turbo:submit-end', () => {
+        this.dispatch('updateSection', { detail: { sectionId: this.sectionIdValue } })
+      }, { once: true })
+    }
+    
+    this.element.requestSubmit()
   }
 }
